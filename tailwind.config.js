60b719d5a1f12658417a17686970adf102779cad
Ceferino Jumao-as V <csjumaoas@up.edu.ph>
2025-07-07 17:20:11 +0800

    Added color paletter config and web background

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,31 @@ export default {
     extend: {
       colors: {
         primary: {
-          300: '#10D2D2',
-          200: '#BDF0F0',
-          100: '#AFEEEE',
-          400: '#3BF4F4'
+          pink: '#CE2C84',
+          orange: '#EE7002',
+          brown: '#402806',
+          black: '#3BF4F4',
+          white: '#FBFBFB',
+          red: '#D63E3E',
+          blue: '#3E85D6',
+          green: '#26B98A'
+        },
+        secondary: {
+          pink: '#EFBBD7',
+          orange: '#FFE5CE',
+          brown: '#FFF1C5',
+          black: '#7B7D7F',
+          red: '#FFCECF',
+          blue: '#CEE3FF',
+          green: '#E7F8F2',
+        },
+        tint: {
+          pink: '#FFCEF3',
+          brown: '#FEF4EB',
+        },
+        stroke: {
+          200: '#DFDFDF',
+          100: '#F1F1F1',
         }
       },
     },
