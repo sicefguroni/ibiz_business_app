0238e3a2a8fdd6059cc3e9f1b321a552bc764a9e
Ceferino Jumao-as V <csjumaoas@up.edu.ph>
2025-07-09 22:51:26 +0800

    Added pages and cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import HomePage from './pages/HomePage';
+import GuidesPage from './pages/GuidesPage';
+import TrainingsPage from './pages/TrainingsPage';
+import LoansPage from './pages/LoansPage';
+import CommunityPage from './pages/CommunityPage';
 
 function App() {
   return (
-    <>
-
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/guides" element={<GuidesPage />} />
+        <Route path="/trainings" element={<TrainingsPage />} />
+        <Route path="/loans" element={<LoansPage />} />
+        <Route path="/community" element={<CommunityPage />} />
+      </Routes>
+    </Router>
   );
 }
 
